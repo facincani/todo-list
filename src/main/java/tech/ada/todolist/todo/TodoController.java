5f607e533fe672977e1d65cf046653ce83699209
facincani <alexfacincani@gmail.com>
2024-03-01 20:13:33 -0300

    metodos http

--- src/main/java/tech/ada/todolist/todo/TodoController.java
+++ src/main/java/tech/ada/todolist/todo/TodoController.java
@@ -1,6 +1,7 @@
 package tech.ada.todolist.todo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,10 +9,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/todo")
 public class TodoController {
 
-
     @GetMapping("/mock")
     public String tarefaMockada(){
         return "tarefa mockada";
+    }
+
+    @GetMapping
+    public String salvarTarefa(){
+        return "tarefa salva";
+    }
+
+    @PostMapping
+    public String criaTarefa(){
+        return "post";
+    }
+
+    @PostMapping("/usuario")
+    public String criarUsuario(){
+        return "";
     }
 
 }

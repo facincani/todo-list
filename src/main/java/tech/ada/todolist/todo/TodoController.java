3e5fe9144679b1f25633c8ecb391f7649e0fd18d
facincani <alexfacincani@gmail.com>
2024-03-04 21:05:42 -0300

    adionando repository

--- src/main/java/tech/ada/todolist/todo/TodoController.java
+++ src/main/java/tech/ada/todolist/todo/TodoController.java
@@ -1,25 +1,21 @@
 package tech.ada.todolist.todo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/todo")
 public class TodoController {
 
-//    @Autowired
     private TodoService service;
 
     public TodoController(TodoService service){
         this.service = service;
     }
 
-    @GetMapping("/all")
-    public List<Tarefa> recuperarTarefas(){
-        return service.getAll();
-    }
+//    @GetMapping("/all")
+//    public List<Tarefa> recuperarTarefas(){
+//        return service.getAll();
+//    }
 
     @GetMapping
     public String salvarTarefa(){
--- src/main/java/tech/ada/todolist/todo/TodoService.java
+++ src/main/java/tech/ada/todolist/todo/TodoService.java
@@ -2,19 +2,20 @@ package tech.ada.todolist.todo;
 
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Service
 public class TodoService {
 
-    private List<Tarefa> repository = new ArrayList<>();
+    private TarefaRepository repository;
+
+    public TodoService(TarefaRepository repository){
+        this.repository = repository;
+    }
 
     public void salvarTarefa(Tarefa tarefa){
-        repository.add(tarefa);
+        repository.save(tarefa);
     }
 
-    public List<Tarefa> getAll() {
-        return repository;
-    }
+//    public List<Tarefa> getAll() {
+//        return repository;
+//    }
 }

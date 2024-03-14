5ae4de0a0a75b269a7e64d5d4aa0f4a979d87ef5
facincani <alexfacincani@gmail.com>
2024-03-13 21:47:47 -0300

    incluindo busca por atributo e ignore case

--- src/main/java/tech/ada/todolist/todo/TarefaRepository.java
+++ src/main/java/tech/ada/todolist/todo/TarefaRepository.java
@@ -1,6 +1,12 @@
 package tech.ada.todolist.todo;
 
 import org.springframework.data.repository.CrudRepository;
+import tech.ada.todolist.usuario.UsuarioEntity;
+
+import java.util.List;
 
 public interface TarefaRepository extends CrudRepository<Tarefa, Integer> {
+
+    List<UsuarioEntity> findByNameIgnoreCaseContaining(String name);
+
 }
